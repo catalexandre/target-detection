6ef063c70c971aaff2ad321cf84831660f577348
Alexandre Catellier <catellier.alexandre@gmail.com>
2024-07-25 21:27:53 -0400

    binarize now returns an array that holds the image data

--- target detection.cpp
+++ target detection.cpp
@@ -12,21 +12,36 @@ bool rSubGB(double r, double g, double b)
     return (value > threshold) ? true : false;
 }
 
-int binarize(string name, bool (*functionRGB)(double, double, double))
+bool test(double r, double g, double b)
+{
+    double threshold = 0.2 * 255;
+    double value = r - 0.3 * g - 0.3 * b;
+
+    return (value > threshold) ? true : false;
+}
+
+int* binarize(string name, bool (*functionRGB)(double, double, double))
 {
     ifstream image(name + ".ppm");
     ofstream binaryImage(name + "Binary.ppm");
 
     if (!image.is_open())
     {
-        return 1;
+        return nullptr;
     }
 
     image.ignore(256, '\n');
 
-    int width, height, maxValue;
+    int maxValue, width, height;
 
     image >> width >> height >> maxValue;
+
+    int imageSize = width * height;
+
+    int* returnData = new int[imageSize + 2];
+
+    returnData[0] = width;
+    returnData[1] = height;
 
     binaryImage << "P1" << endl << width << ' ' << height << endl;
 
@@ -40,32 +55,68 @@ int binarize(string name, bool (*functionRGB)(double, double, double))
 
             double valueCheck = r - 0.25 * g - 0.25 * b;
 
+            int pixelValue;
+
             if (functionRGB(r, g, b))
             {
-                binaryImage << 0 << ' ';
+                pixelValue = 1;
             }
 
             else
             {
-                binaryImage << 1 << ' ';
+                pixelValue = 0;
             }
+
+            binaryImage << pixelValue << ' ';
+            returnData[i * width + j + 2] = pixelValue;
         }
     }
 
     image.close();
     binaryImage.close();
-}
 
-void getTargetPosition()
-{
-
+    return returnData;
 }
 
 int main()
 {
-    if (binarize("Blob_DetectionP3", *rSubGB))
-    {
+    int* binaryImageArray = binarize("Blob_DetectionP3", test);
 
+    if (binaryImageArray != nullptr)
+    {
+        int imageWidth = binaryImageArray[0];
+        int imageHeight = binaryImageArray[1];
+        int imageSize = imageWidth * imageHeight;
+
+        // Allocate a new array for the actual binary image data
+        int* binaryImageData = new int[imageSize];
+
+        // Copy data from old array to new array
+        for (int i = 0; i < imageSize; i++)
+        {
+            binaryImageData[i] = binaryImageArray[2 + i];
+        }
+
+        // Deallocate the old array
+        delete[] binaryImageArray;
+
+        // Print the binary image data
+        for (int i = 0; i < imageHeight; i++)
+        {
+            for (int j = 0; j < imageWidth; j++)
+            {
+                cout << binaryImageData[i * imageWidth + j] << ' ';
+            }
+            cout << endl;
+        }
+
+        // Deallocate the new array
+        delete[] binaryImageData;
+    }
+
+    else
+    {
+        cout << "Failed to binarize the image." << endl;
     }
 
     return 0;
